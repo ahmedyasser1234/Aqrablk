cccc5575116ccbc967924ffae86a865f63124612
ahmedyasser1234 <44351191+ahmedyasser1234@users.noreply.github.com>
2026-01-19 04:22:09 +0200

    Update MontagePage.tsx

--- pages/MontagePage.tsx
+++ pages/MontagePage.tsx
@@ -47,7 +47,7 @@ const MontagePage: React.FC = () => {
           top: -50%;
           left: -50%;
           /* تم تقليل الشفافية قليلاً وزيادة كثافة اللون للظهور مع البلور */
-          background: conic-gradient(transparent 30%, transparent 50%, var(--glow-color));
+          background: conic-gradient(transparent 10%, transparent 20%, var(--glow-color));
           /* تسريع الحركة من 2.5s إلى 1.5s */
           animation: border-rotate 4.0s linear infinite;
           filter: blur(10px);
